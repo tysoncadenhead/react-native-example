33089afea24b2a025bfa167e6cc3406a47f7aa1b
Tyson Cadenhead <tysoncadenhead@gmail.com>
2017-09-19 11:26:53 -0500

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import App from './src/App';
+import {
+  AppRegistry,
+} from 'react-native';
+import React from 'react';
+
+AppRegistry.registerComponent('StarWarsApp', () => App);
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native';
+
+import React from 'react';
+
+const App = () => (
+    <View style={{
+        marginTop: 100
+    }}>
+        <Text style={{
+            textAlign: 'center'
+        }}>Star Wars App</Text>
+    </View>
+);
+
+export default App;
